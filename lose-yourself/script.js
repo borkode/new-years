07588a9767fb2bf75b9ca6629cc26a552e38b4f3
borkode <31637006+borkode@users.noreply.github.com>
2018-12-03 10:07:16 -0500

    Update script.js

--- lose-yourself/script.js
+++ lose-yourself/script.js
@@ -4,7 +4,6 @@ setInterval(function(){
 var ms   = Date.parse(new Date())/1000;
 var ems  = Date.parse("Jan 01 2019 0:00:00")/1000
 var until= (ems-ms);
-console.log(until)
 if(until==59){
 audio.play()
 }
