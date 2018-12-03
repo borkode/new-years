a333a6777ee7efe3aac72b39aa0a1449f20dea4a
borkode <31637006+borkode@users.noreply.github.com>
2018-12-02 20:15:49 -0500

    Update script.js

--- lose-yourself/script.js
+++ lose-yourself/script.js
@@ -8,4 +8,13 @@ console.log(until)
 if(until==59){
 audio.play()
 }
+  
+  var s = (Date.parse(new Date("Jan 01 2019 0:00:00"))-Date.parse(new Date()))/1000
+    var m = Math.floor(s/60)
+    var h = Math.floor(m/60)
+    var d = Math.floor(h/24)
+    document.getElementById('dy').innerHTML=d
+    document.getElementById('hr').innerHTML=h
+    document.getElementById('mn').innerHTML=m
+    document.getElementById('sc').innerHTML=s
 },1);
